61f436103f55375737088d2ad822750e84055dbe
Eduardo Barboza <barboza.edu15@gmail.com>
2021-08-18 20:28:53 -0300

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,10 @@
 import 'package:flutter/material.dart';
+import 'TelaTodosPlantoes.dart';
+import 'alistatelas.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      title: 'Flutter demo',
-      debugShowCheckedModeBanner: false,
-      home: new HomePage(),
-    ),
-  );
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: buildAppBar(), body: buildBody());
-  }
-}
-
-buildAppBar() {
-  return AppBar(
-    backgroundColor: Colors.purple[800],
-    title: Align(
-      alignment: Alignment.center,
-      child: Text('Plantões Cadastrados'),
-    ),
-  );
-}
-
-buildBody() {
-  return ListView(
-    children: [
-      buildListTitle(
-        titulo: 'Plantão a',
-        doutor: 'Doutora a',
-        turno: '8h/12h',
-      ),
-    ],
-  );
-}
-
-buildListTitle({String titulo = '', String doutor = '', String turno = ''}) {
-  return ListTile(
-    title: Text(titulo),
-    subtitle: Text(doutor),
-    trailing: Text(turno),
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: CaixaDeEntrada(),
+  ));
 }
