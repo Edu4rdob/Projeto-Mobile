c7f9156974e291a157563440653dd2edfe0536e3
Eduardo Barboza <barboza.edu15@gmail.com>
2021-08-20 14:27:58 -0300

    finalizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/telas_plantoes_ativos.dart';
+//import 'package:flutter_application_1/telas_plantoes_ativos.dart';
 //import 'package:flutter_application_1/tela_plantoes_registrados.dart';
 //import 'package:flutter_application_1/Historico.dart';
 //import 'package:flutter_application_1/cadastro_pt2.dart';
 //import 'package:flutter_application_1/Cadastro2.dart';
 //import 'package:flutter_application_1/tela_Gerenc_plantoes.dart';
 //import 'package:flutter_application_1/chat.dart';
-//import 'package:flutter_application_1/tela_login.dart';
+import 'package:flutter_application_1/tela_login.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -16,5 +16,5 @@ void main() {
       //home: TelaGerenciarPlantoes(),
       //home: TelaPlantoesRegistrados()
       //home: TelaPlantoes()
-      home: TelaPlantoes()));
+      home: TelaLoginPage()));
 }
