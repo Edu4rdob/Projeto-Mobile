b49ba9720d49579ce1fc7484d2c7318550383773
Eduardo Barboza <barboza.edu15@gmail.com>
2021-08-20 08:40:11 -0300

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/TelaPlantoesAtivos.dart';
-import 'package:flutter_application_1/TelaLogin.dart';
-import 'TelaGerPlantoes.dart';
-import 'troca.dart';
-import 'validar_email.dart';
-import 'TelaChat.dart';
+
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: TelaPlantoes(),
+    home: //TelaPlantoes(),
   ));
 }
