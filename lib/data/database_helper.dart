81f250b84a49bb642a009fae5582d5758a43c288
isabelvdsg <86683266+isabelvdsg@users.noreply.github.com>
2021-10-23 13:33:00 -0300

    Update database_helper.dart

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -29,11 +29,11 @@ class DatabaseHelper {
 
   Future<FutureOr<void>> _onCreate(Database db, int version) async {
     String sql =
-        'create table cadastro_p2 (data_nascimento varchar(10), cpf varchar(11) PRIMARY KEY, telefone varchar(12), area_atuacao varchar(100));';
+        'create table usuario (nome VARCHAR(100), nome_use VARCHAR(100), senha VARCHAR(8), data_nascimento varchar(10), cpf varchar(11) PRIMARY KEY, telefone varchar(12), area_atuacao varchar(100));';
     await db.execute(sql);
 
     sql =
-        "INSERT INTO cadastro_p2 (data_nascimento, cpf, telefone, area_atuacao) VALUES ('11/09/2001', '11122233345', '082999887766', 'Cardiologia');";
+        "INSERT INTO usuario (nome, nome_use, senha, data_nascimento, cpf, telefone, area_atuacao) VALUES ('MARIE CURIE', 'marie_curie02', '12345678', '11/09/2001', '11122233345', '082999887766', 'Cardiologia');";
     await db.execute(sql);
   }
 }
