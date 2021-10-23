68ffe498a496ef3643904a2cdf0d0b9a3518f28d
isabelvdsg <86683266+isabelvdsg@users.noreply.github.com>
2021-10-23 14:31:41 -0300

    Create plantoes_dao.dart

---
+++ lib/data/plantoes_dao.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PlantoesDao {
+  carregarPlantoes() {
+    Future<List<Plantoes>> carregarPlantoes() async {
+      List<Plantoes> listaPlantoes = <Plantoes>[];
+    }
+
+    DatabaseHelper databaseHelper = DatabaseHelper();
+    Database db = await databaseHelper.db;
+
+    String sql = 'SELECT * FROM plantoes';
+    final result = await db.rawQuery(sql);
+
+    print(result);
+
+    for (var json in result) {
+      Plantoes plantao = Plantoes.fromJson(json);
+      listaPlantoes.add(plantao);
+    }
+
+    return listaPlantoes;
+  }
+}
