0e2be6e9740c599c0603096d533a11ac56bac1ee
Eduardo Barboza <barboza.edu15@gmail.com>
2021-12-30 15:11:45 -0300

    ajustando o sharedpreference

--- lib/data/shared_preferences_helper.dart
+++ lib/data/shared_preferences_helper.dart
@@ -1,35 +1,35 @@
 import 'package:flutter_application_1/data/models/usuario.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class SharedPreferencesHelper{
-      Future<SharedPreferences> get _getInstance => SharedPreferences.getInstance();
+class SharedPreferencesHelper {
+  Future<SharedPreferences> get _getInstance => SharedPreferences.getInstance();
 
-      Map<String, String> usuario ={
-        'user': 'marie_curie02',
-        'senha': '12345678',
-      }; 
+  Map<String, String> usuario = {
+    'user': '',
+    'senha': '',
+  };
 
-      setUser(String user, String senha) async{
-        SharedPreferences instance = await _getInstance;
-        await instance.setString('user', user);
-        await instance.setString('senha', senha);
-        await getUser();
-      }
+  setUser(String user, String senha) async {
+    SharedPreferences instance = await _getInstance;
+    await instance.setString('user', user);
+    await instance.setString('senha', senha);
+    await getUser();
+  }
 
-       getUser() async{
-         SharedPreferences instance = await _getInstance;
-         final user = instance.getString('user') ?? 'marie_curie02';
-         final senha = instance.getString('senha') ?? '12345678';
-          usuario = {'user': user, 'senha': senha,};
-          return usuario;
-         
-       }
-       sair() async{
-        SharedPreferences instance = await _getInstance;
-        await instance.remove('user');
-        await instance.remove('senha');
+  getUser() async {
+    SharedPreferences instance = await _getInstance;
+    final user = instance.getString('user') ?? '';
+    final senha = instance.getString('senha') ?? '';
+    usuario = {
+      'user': user,
+      'senha': senha,
+    };
+    return usuario;
+  }
 
-       } 
-
-
+  sair() async {
+    SharedPreferences instance = await _getInstance;
+    await instance.remove('user');
+    await instance.remove('senha');
+  }
 }
